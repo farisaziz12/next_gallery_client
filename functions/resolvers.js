480e57bc04759da6c21d6a77247719b36a72111c
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2021-01-31 23:31:58 +0100

    refactor(functions-resolvers): add lazy loading, fade and fix functionality

--- functions/resolvers.js
+++ functions/resolvers.js
@@ -2,22 +2,32 @@ import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
 import FadeIn from "react-fade-in";
 import LazyLoad from "react-lazyload";
-import Imgix from "react-imgix";
 import { getSlideshowPaths, getRailPaths } from "./";
 import "react-awesome-slider/dist/styles.css";
 import styles from "../styles/Home.module.css";
 
+import { useRouter } from "next/router";
+
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
-export const resolveSlideshow = (title, autoplay, media) => {
-  const images = getSlideshowPaths(media);
+export const resolveSlideshow = (
+  title,
+  autoplay,
+  media,
+  auto_media_select,
+  railMedias
+) => {
+  const images = auto_media_select
+    ? getSlideshowPaths(railMedias)
+    : getSlideshowPaths(media);
 
   return (
-    <FadeIn delay={400}>
+    <FadeIn>
       <div className={styles.slideshow}>
         <AutoplaySlider
           play={autoplay}
           interval={3000}
+          bullets={false}
           media={images}
           style={{ top: "0", position: "relative" }}
         />
@@ -31,19 +41,14 @@ export const resolveRail = (media) => {
   const images = getRailPaths(media);
 
   return (
-    <LazyLoad>
+    <LazyLoad height={400}>
       <FadeIn delay={400}>
         <div className={styles["gallery"]}>
           {images.map((image) => (
-            <Imgix
+            <img
               src={image.src}
-              key={image.id}
-              imgixParams={{
-                fit: "crop",
-                fm: "jpg",
-              }}
-              width="600"
-              height="600"
+              key={image.src}
+              onClick={() => console.log(image.id)}
             />
           ))}
         </div>
